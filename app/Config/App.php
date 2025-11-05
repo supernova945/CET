9a58862d84d4a265fc863967d245473925ae2136
supernova945 <92908665+supernova945@users.noreply.github.com>
2025-11-05 08:25:37 -0600

    Change baseURL to use HTTPS for deployment

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/CET/';
+    public string $baseURL = 'https://supernova945.github.io/CET/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
